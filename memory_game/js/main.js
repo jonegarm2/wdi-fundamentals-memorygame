171c8d99b86e23684810685c73aa418b6abeded3
Jonathan Garman <jonjon@Taylors-MacBook-Pro-2.local>
2018-03-07 09:41:15 -0800

    final version memory game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,24 @@
 console.log ("Up and running!");
-
 var cards = [
 {
     rank: "queen",
     suit: "hearts",
-    cardImage:"../images/queen-of-hearts.png",
+    cardImage:"images/queen-of-hearts.png",
 },
 {
     rank: "queen",
     suit: "diamonds",
-    cardImage: "../images/queen-of-diamonds.png",
+    cardImage: "images/queen-of-diamonds.png",
 },
 {
     rank: "king",
     suit: "hearts",
-    cardImage: "../images/king-of-hearts.png",
+    cardImage: "images/king-of-hearts.png",
 },
 {
     rank: "king",
     suit: "diamonds",
-    cardImage: "../images/king-of-diamonds.png"
+    cardImage: "images/king-of-diamonds.png"
 },
 ];
 var cardsInPlay = [];
@@ -46,7 +45,7 @@ var flipCard = function() {
 var createBoard = function(){
     for (var i = 0; i < cards.length; i++) {
         var cardElement = document.createElement('img');
-        cardElement.setAttribute('src', "../images/back.png");
+        cardElement.setAttribute('src', "./images/back.png");
         cardElement.setAttribute('data-id', i);
         cardElement.addEventListener('click', flipCard);
         
